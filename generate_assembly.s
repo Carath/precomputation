7520ea0fd7475675e578e4197ef54d668a4c42b4
Carath <florian.sananes@laposte.net>
2021-03-07 23:25:23 +0100

    Updated assembly example

--- generate_assembly.s
+++ generate_assembly.s
@@ -1,35 +1,37 @@
 	.file	"generate_assembly.c"
 	.text
-	.p2align 4,,15
+	.p2align 4
 	.globl	generate_asm_approximation
 	.type	generate_asm_approximation, @function
 generate_asm_approximation:
-.LFB4752:
+.LFB5280:
 	.cfi_startproc
+	endbr64
 	vmulss	8(%rdi), %xmm0, %xmm0
 	movl	$0, %edx
 	vaddss	4(%rdi), %xmm0, %xmm0
-	vcvttss2si	%xmm0, %eax
+	vcvttss2sil	%xmm0, %eax
 	testl	%eax, %eax
 	cmovs	%edx, %eax
-	cmpl	%eax, (%rdi)
 	movq	16(%rdi), %rdx
+	cmpl	%eax, (%rdi)
 	cmovle	(%rdi), %eax
 	cltq
 	vmovss	(%rdx,%rax,4), %xmm0
 	ret
 	.cfi_endproc
-.LFE4752:
+.LFE5280:
 	.size	generate_asm_approximation, .-generate_asm_approximation
-	.p2align 4,,15
+	.p2align 4
 	.globl	generate_asm_approx_simd
 	.type	generate_asm_approx_simd, @function
 generate_asm_approx_simd:
-.LFB4753:
+.LFB5281:
 	.cfi_startproc
+	endbr64
 	vbroadcastss	8(%rdi), %xmm0
-	vmulps	(%rdx), %xmm0, %xmm0
 	vbroadcastss	4(%rdi), %xmm1
+	vmulps	(%rdx), %xmm0, %xmm0
 	movq	16(%rdi), %rdx
 	vaddps	%xmm1, %xmm0, %xmm0
 	vpxor	%xmm1, %xmm1, %xmm1
@@ -55,7 +57,23 @@ generate_asm_approx_simd:
 	vmovss	%xmm0, 12(%rsi)
 	ret
 	.cfi_endproc
-.LFE4753:
+.LFE5281:
 	.size	generate_asm_approx_simd, .-generate_asm_approx_simd
-	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
 	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
